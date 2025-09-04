8a64133c2768932295a12d9fcafee6f9e2927a89
Danish Akhtar <221fa04296@vignan.ac.in>
2025-09-05 00:24:57 +0530

    Update app.py

--- worker-python/app.py
+++ worker-python/app.py
@@ -1,24 +1,28 @@
-import os
-from flask import Flask, request, jsonify
+from flask import Flask, request, send_file
 from PIL import Image
+from io import BytesIO
 
 app = Flask(__name__)
-OUTPUT_FOLDER = '/app/output'
-if not os.path.exists(OUTPUT_FOLDER):
-    os.makedirs(OUTPUT_FOLDER)
 
 @app.route('/process', methods=['POST'])
 def process_image():
     if 'image' not in request.files:
-        return jsonify({"error": "No image file provided"}), 400
+        return "No image file provided", 400
 
     file = request.files['image']
     try:
         image = Image.open(file.stream).convert('L') # Grayscale
-        image.save(os.path.join(OUTPUT_FOLDER, 'processed.png'))
-        return jsonify({"message": "Success! Image is now grayscale."}), 200
+
+        # Save image to an in-memory buffer
+        buffer = BytesIO()
+        image.save(buffer, format="PNG")
+        buffer.seek(0)
+
+        # Send the image file back in the response
+        return send_file(buffer, mimetype='image/png')
+
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        return str(e), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
